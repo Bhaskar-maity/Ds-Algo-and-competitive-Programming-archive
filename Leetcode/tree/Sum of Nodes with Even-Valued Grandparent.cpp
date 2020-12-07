10f94798eeecfbf671c4826a088a0fb2a58d64c6
Bhaskar Maity <maitybhaskar2015@gmail.com>
2020-12-07 23:17:02 +0530

    Sum of Nodes with Even-Valued Grandparent

---
+++ Leetcode/tree/Sum of Nodes with Even-Valued Grandparent.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    int sumEvenGrandparent(TreeNode* root) {
+        dfs(root,NULL,NULL);
+        return sum;
+    }
+    
+    void dfs(TreeNode* curr, TreeNode* parent, TreeNode* grandParent){
+        if(curr==NULL) return;
+        if(grandParent != NULL && grandParent->val % 2 == 0)
+            sum += curr->val;
+        dfs(curr->left, curr, parent);
+        dfs(curr->right, curr, parent);
+    }
+    
+};
