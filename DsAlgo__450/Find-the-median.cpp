212e3aa18c8dfe45acdf5a663bb53d6fe88b5492
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-01-26 20:32:25 +0530

    Find the median

---
+++ DsAlgo__450/Find-the-median.cpp
@@ -0,0 +1,13 @@
+int find_median(vector<int> v)
+{
+    int n=v.size();
+    sort(v.begin(),v.end());
+    
+    if(n%2==0){
+        return (v[(n/2)-1]+v[n/2])/2;
+    }
+    else{
+        return v[n/2];
+    }
+}
+
