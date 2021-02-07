d877c68c3120d6892bf1444e7f0ecc2c6808c1f1
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-02-07 20:21:21 +0530

    Nearest_greater_element_left

---
+++ DsAlgo__450/Stack/Nearest_greater_eliment_left.cpp
@@ -0,0 +1,70 @@
+//using stack o(n) solution
+
+
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std; 
+void printArray(int arr[], int size)  
+{  
+    int i;  
+    for (i = 0; i < size; i++)  
+        cout << arr[i] << " ";  
+    cout << endl;  
+}
+
+
+vector<int> nearest_greater_to_left(int a[], int n){
+    vector<int> v;
+    stack<int> s;
+
+    for(int i = 0; i < n; i++){
+        if(s.size()==0){
+            v.push_back(-1);
+
+        }
+
+        else if(s.size() > 0 && a[i]<s.top()){
+            v.push_back(s.top());
+        }
+
+        else if(s.size() > 0 && a[i]>=s.top()){
+            while(s.size() > 0 && a[i]>=s.top())
+             s.pop();
+
+            if(s.size()==0){
+                v.push_back(-1);
+
+            }
+
+            else {
+                v.push_back(s.top());
+            }
+        }
+
+        s.push(a[i]);
+    }
+
+    return v;
+}
+
+int main() 
+{ 
+    int price[] = { 2, 30, 15, 10, 8, 25, 80 }; 
+    
+    int n = sizeof(price) / sizeof(price[0]); 
+    printArray(price, n);
+    cout << "Nearest_greater_element_left = " ;
+    vector<int> v;
+    v = nearest_greater_to_left(price, n); 
+     
+    
+
+    for(int i = 0; i < n; i++)
+	{
+		cout << v[i] << " ";
+        
+        		
+	}
+
+    return 0;
+}
