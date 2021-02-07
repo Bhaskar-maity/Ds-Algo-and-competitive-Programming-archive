1eaa703f9e9aeee4ae43d52a584ac07da18ae5e3
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-02-07 20:16:17 +0530

    Nearest_small_eliment_right

---
+++ DsAlgo__450/Stack/Nearest_small_eliment_right.cpp
@@ -0,0 +1,73 @@
+//using stack o(n) solution
+
+
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std; 
+
+void printArray(int arr[], int size)  
+{  
+    int i;  
+    for (i = 0; i < size; i++)  
+        cout << arr[i] << " ";  
+    cout << endl;  
+}
+
+
+vector<int> nearest_smaller_to_right(int a[], int n){
+    vector<int> v;
+    stack<int> s;
+
+    for(int i = n-1; i >=0; i--){
+        if(s.size()==0){
+            v.push_back(-1);
+
+        }
+
+        else if(s.size() > 0 && a[i]>s.top()){
+            v.push_back(s.top());
+        }
+
+        else if(s.size() > 0 && a[i]<=s.top()){
+            while(s.size() > 0 && a[i]<=s.top())
+             s.pop();
+
+            if(s.size()==0){
+                v.push_back(-1);
+
+            }
+
+            else {
+                v.push_back(s.top());
+            }
+        }
+
+        s.push(a[i]);
+    }
+
+    reverse(v.begin(), v.end());
+
+    return v;
+}
+
+int main() 
+{ 
+    int price[] = { 2, 30, 15, 10, 8, 25, 80 }; 
+    
+    int n = sizeof(price) / sizeof(price[0]); 
+    printArray(price, n);
+    cout << "Nearest_small_eliment_right = " ;
+    vector<int> v;
+    v = nearest_smaller_to_right(price, n); 
+     
+    
+
+    for(int i = 0; i < n; i++)
+	{
+		cout << v[i] << " ";
+        
+        		
+	}
+
+    return 0;
+}
