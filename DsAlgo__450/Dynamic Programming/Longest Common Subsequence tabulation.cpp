e5e707d69f38c86455bdd01cce397db4b00286e3
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-02-21 20:30:48 +0530

    Longest Common Subsequence tabulation gfg-43 d-52

---
+++ DsAlgo__450/Dynamic Programming/Longest Common Subsequence tabulation.cpp
@@ -0,0 +1,25 @@
+//https://practice.geeksforgeeks.org/problems/longest-common-subsequence-1587115620/1
+//tabulation approach optimal
+
+int lcs(int x, int y, string s1, string s2){
+    int t[x+1][y+1];
+    for(int i = 0; i <= x; i++)
+        t[i][0] = 0;
+        
+    for(int j = 0; j <= y; j++)
+        t[0][j] = 0;
+
+    for(int i = 1; i <= x; i++)
+    {
+        for(int j = 1; j <= y; j++)
+        {
+            if(s1[i-1]==s2[j-1]){
+                t[x][y] = 1 + t[x-1][y-1];
+            }
+            else {
+                t[x][y] = max(t[x-1][y], t[x][y-1]);
+            }
+        }
+    }
+    return t[x][y];
+}
