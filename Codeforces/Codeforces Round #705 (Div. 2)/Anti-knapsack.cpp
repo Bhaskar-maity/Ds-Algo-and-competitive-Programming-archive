177bef25496415f1399a22bb5c60a199ce7315a9
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-03-07 12:32:37 +0530

    Anti-knapsack codeforces

---
+++ Codeforces/Codeforces Round #705 (Div. 2)/Anti-knapsack.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+const int max_n = -1, inf = 1000111222;
+ 
+int t, n, k;
+ 
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    cin >> t;
+    while (t--) {
+        cin >> n >> k;
+        cout << n - max(1, (k + 1) / 2) << "\n";
+        for (int i = max(1, (k + 1) / 2); i <= n; ++i) {
+            if (i != k) {
+                cout << i << " ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
