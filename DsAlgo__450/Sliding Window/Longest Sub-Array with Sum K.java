333c62f88d44de32c195a1a758aae5d8b71921e1
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-03-20 20:34:09 +0530

    Longest Sub-Array with Sum K gfg d-76

--- DsAlgo__450/Sliding Window/Longest Sub-Array with Sum K.java
+++ DsAlgo__450/Sliding Window/Longest Sub-Array with Sum K.java
@@ -1,23 +1,14 @@
-// https://practice.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1#
-// Input :
-// A[] = {10, 5, 2, 7, 1, 9}
-// K = 15
-// Output : 4
-// Explanation:
-// The sub-array is {5, 2, 7, 1}.
+//User function Template for Java
 
 class Solution{
     
    
     // Function for finding maximum and value pair
     public static int lenOfLongSubarr (int A[], int N, int K) {
-        //sliding window
-        int i=0,j=0;
-        long sum=0;
+        int i=0,j=0, sum=0;
         int mx=Integer.MIN_VALUE;
-        
         while(j<N){
-            sum= sum + A[j];
+            sum+=A[j];
 
             if(sum < K){
                 j++;
@@ -28,17 +19,14 @@ class Solution{
             }
             else if(sum > K){
                 while(sum>K){
-                    sum= sum - A[i];
+                    sum-=A[i];
                     i++;
                 }
                 j++;
             }
         }
-        
-        //if sum != K, in the end of the loop
-        if(mx == Integer.MIN_VALUE) return 0;
-        else
-            return mx;
+
+        return mx;
     }
     
     
