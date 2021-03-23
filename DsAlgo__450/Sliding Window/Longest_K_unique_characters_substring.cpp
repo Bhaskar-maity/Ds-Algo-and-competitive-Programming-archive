b392f6cfb8e3e3c0257a44f34933805aea08f9ff
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-03-23 21:08:40 +0530

    Longest K unique characters substring gfg d-78

--- DsAlgo__450/Sliding Window/Longest_K_unique_characters_substring.cpp
+++ DsAlgo__450/Sliding Window/Longest_K_unique_characters_substring.cpp
@@ -5,33 +5,50 @@
 // Explanation: "cbebebe" is the longest 
 // substring with K distinct characters.
 
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+int longestKSubstr(string s, int k);
+int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    
+    
+        string s;
+        cin >> s;
+        int k;
+        cin >> k;
+
+        cout << longestKSubstr(s, k) << endl;
+    
+}
+*/
 int longestKSubstr(string s, int k) {
     unordered_map<char,int> mp;
-    
-    //sliding window
     int i=0,j=0,cnt=INT_MIN;
     int n = s.size();
     
     while(j<n){
-        //if first time in map then put 1
+       
         if( mp.find(s[j]) == mp.end()){
             mp[s[j]]=1;
         }
         else
             mp[ s[j] ]++;
-        int msize=mp.size();
+        //int msize=mp.size();
         
-        if(msize < k){
+        if(mp.size() < k){
             j++;
         }
 
-        else if(msize == k){
+        else if(mp.size() == k){
             cnt=max(cnt, j-i+1);
             j++;
         }
 
-        else if(msize > k){
-            while(msize > k){
+        else if(mp.size() > k){
+            while(mp.size() > k){
                 mp[s[i]]--;
                 if(mp[s[i]] == 0){
                     mp.erase(s[i]);
@@ -40,7 +57,18 @@ int longestKSubstr(string s, int k) {
             }
             j++;
         }
+        
+        
+        
     }
-
-    return cnt;
+    
+    //if map size never would equalto k, then cnt will remain INT_MIN and
+    //in this case we have return -1
+    // S = "aaaa", K = 2
+    // Output: -1
+    if(cnt == INT_MIN) 
+        return -1;
+    else
+        return cnt;
 }
+
