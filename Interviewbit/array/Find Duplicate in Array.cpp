b583bf97de3c24459228320f0a105385964af6af
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-04-05 22:26:18 +0530

    Find Duplicate in Array d-81

---
+++ Interviewbit/array/Find Duplicate in Array.cpp
@@ -0,0 +1,19 @@
+//https://www.interviewbit.com/problems/find-duplicate-in-array/
+// Sample Input:
+
+// [3 4 1 4 1]
+// Sample Output:
+
+// 1
+
+int Solution::repeatedNumber(const vector<int> &A) {
+    bool a[A.size()] = {false};
+
+    for(int i = 0; i < A.size(); i++){
+        if(a[A[i]] == false){
+            a[A[i]] = true;
+        }
+        else return A[i];
+    }
+    return -1;
+}
