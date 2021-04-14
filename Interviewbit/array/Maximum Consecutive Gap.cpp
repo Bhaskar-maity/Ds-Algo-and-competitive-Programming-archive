ad3cae807defac85cb4bad5bcc73926de0925b26
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-04-14 23:48:51 +0530

    Spiral Order Matrix II d-89

--- Interviewbit/array/Maximum Consecutive Gap.cpp
+++ Interviewbit/array/Maximum Consecutive Gap.cpp
@@ -56,3 +56,58 @@ int Solution::maximumGap(const vector<int> &A) {
     return maxgap;
 
 }
+
+//java
+public class Solution {
+    public ArrayList<ArrayList<Integer>> generateMatrix(int A) {
+        int arr[][] = new int [A][A];
+        // marking boundaries
+        int top=0, bottom=A-1, left=0, right=A-1;
+        int direction = 0, count = 1;
+        
+        while(left<=right && top<=bottom) {
+            if(direction==0) {
+                for(int i=left; i<=right; i++) {
+                    arr[top][i] = count;
+                    count++;
+                }
+                top++;
+                direction = 1;
+            } else if(direction==1) {
+                for(int i=top; i<=bottom; i++) {
+                    arr[i][right] = count;
+                    count++;
+                }
+                right--;
+                direction = 2;
+            } else if(direction==2) {
+                for(int i=right; i>=left; i--) {
+                    arr[bottom][i] = count;
+                    count++;
+                }
+                bottom--;
+                direction = 3;
+            } else if(direction==3) {
+                for(int i=bottom; i>=top; i--) {
+                    arr[i][left] = count;
+                    count++;
+                }
+                left++;
+                direction = 0;
+            } 
+        }
+        
+        // converting spiral array into list of list.
+        ArrayList<ArrayList<Integer>> finalList = new ArrayList();
+        for(int i=0;i<A;i++) {
+            ArrayList<Integer> list = new ArrayList();
+            for(int j=0;j<A;j++) {
+                list.add(arr[i][j]);
+            }
+            finalList.add(list);
+        }
+        
+        return finalList;
+    }
+}
+
