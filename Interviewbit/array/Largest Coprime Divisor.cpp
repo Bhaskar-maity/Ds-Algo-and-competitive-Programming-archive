43ca7c5800ede6642020b9dc52a1f186019c851c
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-05-04 23:15:50 +0530

    Largest Coprime Divisor brute force

---
+++ Interviewbit/array/Largest Coprime Divisor.cpp
@@ -0,0 +1,31 @@
+// https://www.interviewbit.com/problems/largest-coprime-divisor/
+// You are given two positive numbers A and B. You need to find the maximum valued integer X such that:
+
+// X divides A i.e. A % X = 0
+// X and B are co-prime i.e. gcd(X, B) = 1
+// For example,
+
+// A = 30
+// B = 12
+// We return
+// X = 5
+
+int gcd(int a, int b){
+    if(b==0) return a;
+    else {
+        return gcd(b, a%b);
+    }
+}
+
+int Solution::cpFact(int A, int B) {
+    int x=10;
+    for(int i= 1; i<= A; i++){
+
+        if(A % i == 0 && gcd(i,B) == 1){
+                x=i;
+            
+        }
+    }
+
+    return x;
+}
