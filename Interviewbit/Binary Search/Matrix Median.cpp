233ee1efbd2ba6a9a5bc7ea15478472ef21f87f4
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-05-06 20:11:01 +0530

    Matrix Median best approach

--- Interviewbit/Binary Search/Matrix Median.cpp
+++ Interviewbit/Binary Search/Matrix Median.cpp
@@ -26,3 +26,40 @@ vector<int> temp;
      
      return answer;
 }
+
+// 1 2 3 
+// 4 5 7
+// 7 8 9
+//binary approach
+//time - o(n* log(m))
+// space -o(1)
+int Solution::findMedian(vector<vector<int> > &arr)
+{
+    //row
+    int r=arr.size();
+    //col
+    int c=arr[0].size();
+
+    //getting max, min of the array
+    int mx=INT_MIN ,mn=INT_MAX;
+
+    for(int i=0;i<r;i++){
+        mn=min(mn, arr[i][0]);
+        mx =max(mx, arr[i][c-1]);
+    }
+
+    int idx = ((r*c) + 1)/2;
+    while(mn<mx){
+        int mid = (mn + mx)/2;
+        int cnt=0;
+
+        for(int i=0; i<r; i++){
+            cnt+= (upper_bound(arr[i].begin(), arr[i].end(), mid) - arr[i].begin());
+        }
+        if(cnt < idx){
+            mn = mid + 1;
+        }
+        else mx = mid;
+    }
+    return mn;
+}
