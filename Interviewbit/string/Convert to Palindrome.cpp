14a3d80aaef5d6ae4836117eba4be6ee537b6443
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-05-18 22:45:15 +0530

    Convert to Palindrome

--- Interviewbit/string/Convert to Palindrome.cpp
+++ Interviewbit/string/Convert to Palindrome.cpp
@@ -1,23 +1,31 @@
+// //https://www.interviewbit.com/problems/convert-to-palindrome/
+
+// Given a string A consisting only of lowercase characters, we need to check whether it is possible to make this string a palindrome after removing exactly one character from this.
+
+// If it is possible then return 1 else return 0.
+// Input 1:
+
+//  A = "abcba"
+//  Output 1:
+
+//  1
 int Solution::solve(string A) {
     int n=A.size(), cnt=0;
     int i=0, j=n-1;
     if(n==0) return 0;
     while(i<=j){
         if(A[i]!=A[j]){
-            //if i still in 1st position
-            if(i ==0) {
+            
+            if(A[i+1]==A[j]){
+                cnt++;
+                i++;
+            }
+            else if(A[i]==A[j-1]){
                 cnt++;
                 j--;
             }
-            //we have to insert in beginning so i=0, even if when i>0
-            else {
-                //cause when we A[i]=A[j], then that cnt i we have to add, cause now A[i]!=A[j]
-                cnt+=i;
-            }
-            if(cnt>1) {
-                 return 0;
-                 break;
-            }
+            else return 0;
+            
                
             
         }
@@ -25,6 +33,11 @@ int Solution::solve(string A) {
             i++; j--;
         }
     }
-
+    
+    if(cnt>1) {
+                 return 0;
+                 
+            }
+            else
     return 1;
 }
