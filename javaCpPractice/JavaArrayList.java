94f52167561a1f6541bbc485d5a9aab997e34925
Bhaskar Maity <maitybhaskar2015@gmail.com>
2021-08-06 00:02:55 +0530

    java practice

---
+++ javaCpPractice/JavaArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class JavaArrayList {
+    public static void main(String[] args){
+        // Scanner obj = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("'a'");
+        list.add("'gh'");
+        list.add("'sdvgsad'");
+
+        // float x = obj.nextFloat();
+        System.out.println(list);
+    }
+}
---
+++ javaCpPractice/JavaInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class JavaInput {
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+
+        float x = obj.nextFloat();
+        System.out.println(x);
+    }
+}
